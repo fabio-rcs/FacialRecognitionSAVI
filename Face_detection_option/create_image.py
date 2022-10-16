83eb3d217f6e9910b1d5d3034ff44cd077860723
GoncaloR00 <gribeiro@ua.pt>
2022-10-16 15:06:14 +0100

    exemplos

---
+++ Face_detection_option/create_image.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import cv2
+from cv2 import imwrite
+import face_recognition
+
+def main():
+
+    # ---------------------------------------------------------------------------------
+    # Initialization
+    # ---------------------------------------------------------------------------------
+
+    # Object Detection in Real-time
+    # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+
+    #Capture Video from Camera
+    try:
+        cap = cv2.VideoCapture(0)  
+    except:
+        print("Cannot open camera") 
+    if not cap.isOpened():                 
+        print("Cannot open camera") 
+        exit()
+    # ---------------------------------------------------------------------------------
+    # Execution
+    # ---------------------------------------------------------------------------------
+    while True:
+        # Capture frame-by-frame
+        ret, img_RGB = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        
+        # Display the resulting frame
+        cv2.imshow('video', img_RGB)
+
+        if cv2.waitKey(1) == ord('q'):
+            cv2.imwrite('imagem-teste2.png', img_RGB)
+            break
+    # ---------------------------------------------------------------------------------
+    # Termination
+    # ---------------------------------------------------------------------------------
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+#Sees if function was called in the terminal
+if __name__ == "__main__":
+    main()
