d23f5d463459ea1afaa2d7cfae030fb613140bf5
Tatiana <tatianaresende@ua.pt>
2022-10-23 11:40:51 +0100

    updated

--- main_initialization.py
+++ main_initialization.py
@@ -1,67 +1,54 @@
 #!/usr/bin/env python3
 
-""" Falta organizar estes imports 
-	Talvez meter isto por classes para ser mais organizado"""
-
-import tkinter as tk                # python 3
-from tkinter import font as tkfont
-from matplotlib import pyplot as plt
-from matplotlib.pyplot import figure
-import os
-import time
-import numpy as np
 import cv2
-
+import os
+import numpy as np
+import tkinter as tk              
+from matplotlib import pyplot as plt
+from MakeSomething import Make_Something
 # -----------------------------------------------------------
 # Start mode selection, with or without database
 # -----------------------------------------------------------
-BaseD_ON = False
-BaseD_OFF = False 
 
-def make_something(value):
-	global BaseD_OFF
-	global BaseD_ON
-	if value == True:
-		BaseD_ON = True
-		BaseD_OFF = False
-		root.destroy()
-		#print('On ' + str(BaseD_ON) + ' OFF ' + str(BaseD_OFF))
-	else: 
-		BaseD_ON = False
-		BaseD_OFF = True 
-		root.destroy()
-		#print('On ' + str(BaseD_ON) + ' OFF ' + str(BaseD_OFF))
-	
 root = tk.Tk()
 frm = tk.Frame(root)
 frm.grid()
 
+def Value(value):
+	global BaseD_ON
+	global BaseD_OFF
+	value = value
+	BaseD_ON,BaseD_OFF = Make_Something(root,value).make_something()
+
 tk.Label(frm, text="Escolha tipo de inicio").grid(column=0, row=0)
 tk.Label(frm, text="").grid(column=0, row=1)
 
-tk.Button(frm, text="BaseD ON", command = lambda *args: make_something(True)).grid(column=0, row=2)
-tk.Button(frm, text="BaseD OFF", command = lambda *args: make_something(False)).grid(column=0, row=3)
+tk.Button(frm, text="BaseD ON", command = lambda *args: Value(0), activebackground='green').grid(column=0, row=2)
+tk.Button(frm, text="BaseD OFF", command = lambda *args: Value(1), activebackground='green').grid(column=0, row=3)
+tk.Button(frm, text="Espaço Branco", command = lambda *args: Value(2), activebackground='green').grid(column=0, row=4)
 
 root.mainloop()
 
 # -----------------------------------------------------------
 # View database
 # -----------------------------------------------------------
+
 if BaseD_ON == True:
 	dir_images = './Images'
 	images_names = os.listdir(dir_images)
 	num_total_img = np.array(images_names).size
 
+	# ------------------------------------------
+    # Plot with the people in the database
+    # ------------------------------------------
 	id = 0
-	plt.figure(figsize=(2, 16))	
+	plt.figure("DataBase",figsize=(3, 16))
 	for image in images_names:
 		id += 1
 		img = cv2.imread(str('./Images/'+image))
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 		plt.subplot(num_total_img,1,id), plt.imshow(img)
 		plt.title(image.rsplit('.',1)[0]), plt.xticks([]), plt.yticks([])
-		plt.subplots_adjust(top=0.90, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                    wspace=0.35)
+		plt.subplots_adjust(top=0.95, bottom=0.08, right=0.95, hspace=0.25)
 	
 	plt.show()
-
-	
